736eeec97e0a41b23d7bcee2b3ac1fcf541645e0
moshuying <1460083332@qq.com>
2019-11-11 12:57:12 +0800

    减少了冗余 记住js 1/3*3的问题

--- leetcode/36. 有效的数独/index.js
+++ leetcode/36. 有效的数独/index.js
@@ -2,7 +2,7 @@
  * @Description :墨抒颖
  * @Author :墨抒颖
  * @Date :2019-11-10 22:25:48
- * @LastEditTime :2019-11-11 12:53:23
+ * @LastEditTime :2019-11-11 12:55:40
  * @LastEditors :墨抒颖
  * @Github :https://github.com/moshuying
  * @Gitee :https://gitee.com/moshuying
@@ -21,7 +21,7 @@ var isValidSudoku = function (board) {
         columns[i] = {};
         boxes[i] = {};
     }
-    const fun = (r, x, y) => (r[x][y] ? r[x][y]++ : (r[x][y] = 1));
+    // const fun = (r, x, y) => (r[x][y] ? r[x][y]++ : (r[x][y] = 1));
     for (let i = 0; i < board.length; i++) {
         for (let j = 0; j < board[i].length; j++) {
             let num = board[i][j];
@@ -29,9 +29,12 @@ var isValidSudoku = function (board) {
                 let n = parseInt(num, 10);
                 // js中1/3*3计算结果为1 但是按理说应该是0.999无限(浮点误差) 而java中1/3*3则取值为0,浮点数默认舍去0
                 let boxix = Math.floor(Math.floor((i / 3)) * 3) + Math.floor(j / 3);
-                fun(rows, i, n);
-                fun(columns, j, n);
-                fun(boxes, boxix, n);
+                // fun(rows, i, n);
+                // fun(columns, j, n);
+                // fun(boxes, boxix, n);
+                rows[i][n] ? rows[i][n]++ : (rows[i][n] = 1);
+                columns[j][n] ? columns[j][n]++ : (columns[j][n] = 1);
+                boxes[boxix][n] ? boxes[boxix][n]++ : (boxes[boxix][n] = 1);
                 if (rows[i][n] >= 2 || columns[j][n] >= 2 || boxes[boxix][n] >= 2) {
                     return false;
                 }
