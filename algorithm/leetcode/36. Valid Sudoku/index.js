3c9df4908a83f15f6dd5c2ccaccb9b9651e09efd
moshuying <1460083332@qq.com>
2019-11-16 11:42:49 +0800

    更新36题,Math.floor替换为~~两次按位取反获得整数

--- algorithm/leetcode/36. Valid Sudoku/index.js
+++ algorithm/leetcode/36. Valid Sudoku/index.js
@@ -2,7 +2,7 @@
  * @Description :墨抒颖
  * @Author :墨抒颖
  * @Date :2019-11-10 22:25:48
- * @LastEditTime :2019-11-11 13:03:15
+ * @LastEditTime :2019-11-16 11:41:29
  * @LastEditors :墨抒颖
  * @Github :https://github.com/moshuying
  * @Gitee :https://gitee.com/moshuying
@@ -15,44 +15,44 @@
  */
 // eslint-disable-next-line complexity
 var isValidSudoku = function (board) {
-    let [rows, columns, boxes] = [[], [], []];
-    for (let i = 0; i < 9; i++) {
-        rows[i] = {};// 行数据
-        columns[i] = {}; // 列数据
-        boxes[i] = {}; // 小宫格
-    }
-    // const fun = (r, x, y) => (r[x][y] ? r[x][y]++ : (r[x][y] = 1));
-    for (let i = 0; i < board.length; i++) {
-        for (let j = 0; j < board[i].length; j++) {
-            let num = board[i][j];
-            if (num !== '.') {
-                let n = parseInt(num, 10);
-                // js中1/3*3计算结果为1 但是按理说应该是0.999无限(浮点误差) 而java中1/3*3则取值为0,浮点数默认舍去0
-                let boxix = Math.floor((i / 3)) * 3 + Math.floor(j / 3);
-                // fun(rows, i, n);
-                // fun(columns, j, n);
-                // fun(boxes, boxix, n);
-                rows[i][n] ? rows[i][n]++ : (rows[i][n] = 1);
-                columns[j][n] ? columns[j][n]++ : (columns[j][n] = 1);
-                boxes[boxix][n] ? boxes[boxix][n]++ : (boxes[boxix][n] = 1);
-                if (rows[i][n] >= 2 || columns[j][n] >= 2 || boxes[boxix][n] >= 2) {
-                    return false;
-                }
-            }
+  let [rows, columns, boxes] = [[], [], []];
+  for (let i = 0; i < 9; i++) {
+    rows[i] = {};// 行数据
+    columns[i] = {}; // 列数据
+    boxes[i] = {}; // 小宫格
+  }
+  // const fun = (r, x, y) => (r[x][y] ? r[x][y]++ : (r[x][y] = 1));
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      let num = board[i][j];
+      if (num !== '.') {
+        let n = parseInt(num, 10);
+        // js中1/3*3计算结果为1 但是按理说应该是0.999无限(浮点误差) 而java中1/3*3则取值为0,浮点数默认舍去0
+        let boxix = ~~((i / 3)) * 3 + ~~(j / 3);
+        // fun(rows, i, n);
+        // fun(columns, j, n);
+        // fun(boxes, boxix, n);
+        rows[i][n] ? rows[i][n]++ : (rows[i][n] = 1);
+        columns[j][n] ? columns[j][n]++ : (columns[j][n] = 1);
+        boxes[boxix][n] ? boxes[boxix][n]++ : (boxes[boxix][n] = 1);
+        if (rows[i][n] >= 2 || columns[j][n] >= 2 || boxes[boxix][n] >= 2) {
+          return false;
         }
+      }
     }
-    return true;
+  }
+  return true;
 };
 console.log(
-    isValidSudoku([
-        ['5', '3', '.', '.', '7', '.', '.', '.', '.'],
-        ['6', '.', '.', '1', '9', '5', '.', '.', '.'],
-        ['.', '9', '8', '.', '.', '.', '.', '6', '.'],
-        ['8', '.', '.', '.', '6', '.', '.', '.', '3'],
-        ['4', '.', '.', '8', '.', '3', '.', '.', '1'],
-        ['7', '.', '.', '.', '2', '.', '.', '.', '6'],
-        ['.', '6', '.', '.', '.', '.', '2', '8', '.'],
-        ['.', '.', '.', '4', '1', '9', '.', '.', '5'],
-        ['.', '.', '.', '.', '8', '.', '.', '7', '9'],
-    ]),
+  isValidSudoku([
+    ['5', '3', '.', '.', '7', '.', '.', '.', '.'],
+    ['6', '.', '.', '1', '9', '5', '.', '.', '.'],
+    ['.', '9', '8', '.', '.', '.', '.', '6', '.'],
+    ['8', '.', '.', '.', '6', '.', '.', '.', '3'],
+    ['4', '.', '.', '8', '.', '3', '.', '.', '1'],
+    ['7', '.', '.', '.', '2', '.', '.', '.', '6'],
+    ['.', '6', '.', '.', '.', '.', '2', '8', '.'],
+    ['.', '.', '.', '4', '1', '9', '.', '.', '5'],
+    ['.', '.', '.', '.', '8', '.', '.', '7', '9'],
+  ]),
 );
