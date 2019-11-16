397f01b6e1676906b1ae28035fdddeed2dc86255
moshuying <1460083332@qq.com>
2019-11-16 11:54:06 +0800

    完善344题解答

--- algorithm/leetcode/344. Reverse String/index.js
+++ algorithm/leetcode/344. Reverse String/index.js
@@ -2,7 +2,7 @@
  * @Description :墨抒颖
  * @Author :墨抒颖
  * @Date :2019-11-16 10:24:42
- * @LastEditTime :2019-11-16 11:45:50
+ * @LastEditTime :2019-11-16 11:53:26
  * @LastEditors :墨抒颖
  * @Github :https://github.com/moshuying
  * @Gitee :https://gitee.com/moshuying
@@ -14,20 +14,24 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function (s) {
-  // if (s.length > 0) {
-  //   let [i, j] = [0, s.length - 1];
-  //   while (i < j) {
-  //     [s[i], s[j]] = [s[j], s[i]];
-  //     i++;
-  //     j--;
-  //   }
-  // }
-  const swap = (start, s) => {
-    if (start > 0) {
-      [s[start - 1], s[s.length - start]] = [s[s.length - start], s[start - 1]];
-      swap(--start, s);
+  // 双指针
+  if (s.length > 0) {
+    let [i, j] = [0, s.length - 1];
+    while (i < j) {
+      [s[i], s[j]] = [s[j], s[i]];
+      i++;
+      j--;
     }
-  };
-  swap(~~(s.length / 2), s);
+  }
+  // 递归
+  // const swap = (start, s) => {
+  //   if (start > 0) {
+  //     [s[start - 1], s[s.length - start]] = [s[s.length - start], s[start - 1]];
+  //     swap(--start, s);
+  //   }
+  // };
+  // swap(~~(s.length / 2), s);
 };
-console.log(reverseString(['h', 'e', 'l', 'l', 'o']));
+let temp = ['h', 'e', 'l', 'l', 'o'];
+reverseString(temp);
+console.log(temp);
