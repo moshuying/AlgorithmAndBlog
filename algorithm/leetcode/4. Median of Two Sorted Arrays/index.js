4eabbd414077673a37eb070463b81436e1fe9d13
moshuying <1460083332@qq.com>
2019-11-16 12:04:53 +0800

    完善4题代码

---
+++ algorithm/leetcode/4. Median of Two Sorted Arrays/index.js
@@ -0,0 +1,26 @@
+/**
+ * @Description :墨抒颖
+ * @Author :墨抒颖
+ * @Date :2019-11-10 19:19:51
+ * @LastEditTime :2019-11-16 12:03:16
+ * @LastEditors :墨抒颖
+ * @Github :https://github.com/moshuying
+ * @Gitee :https://gitee.com/moshuying
+ * @Blogs :https://blog.csdn.net/qq_34846662
+ * @Use :使用说明
+ */
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function (nums1, nums2) {
+  let temp = nums1.concat(nums2);
+  temp.sort((item1, item2) => item1 - item2);
+  if (temp.length === 1) {return temp[0];}
+  if ((temp.length + 1) % 2 === 0) {
+    return temp[(parseInt(temp.length + 1, 10) / 2 - 1)];
+  } else {
+    return (temp[parseInt((temp.length - 1) / 2, 10)] + temp[parseInt((temp.length - 1) / 2, 10) + 1]) / 2;
+  }
+};
