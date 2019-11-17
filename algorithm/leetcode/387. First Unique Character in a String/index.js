7153e541bd2053bf5fe561e831268312f2aa68b6
moshuying <1460083332@qq.com>
2019-11-17 19:38:53 +0800

    完善387题题解

---
+++ algorithm/leetcode/387. First Unique Character in a String/index.js
@@ -0,0 +1,41 @@
+/**
+ * @Description :墨抒颖
+ * @Author :墨抒颖
+ * @Date :2019-11-17 19:10:43
+ * @LastEditTime :2019-11-17 19:36:55
+ * @LastEditors :墨抒颖
+ * @Github :https://github.com/moshuying
+ * @Gitee :https://gitee.com/moshuying
+ * @Blogs :https://blog.csdn.net/qq_34846662
+ * @Use :使用说明
+ */
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function (s) {
+  if (s === '' || s.length < 1) {return -1;}
+  let set = new Set();
+  for (let i = 0 ;i < s.length;i++) {
+    // position,可选。从当前字符串的哪个索引位置开始搜寻子字符串，默认值为0。
+    if (!s.includes(s[i], i + 1) && !set.has(s[i])) {
+      return i;
+    } else {
+      set.add(s[i]);
+    }
+  }
+  return -1;
+  // if (s === '' || s.length < 1) {return -1;}
+  // let map = new Set();
+  // for (let i = 0 ;i < s.length;i++) {
+  //   map[s[i]] === undefined ? map[s[i]] = 0 : map[s[i]]++;
+  // }
+  // for (const key in map) {
+  //   if (map[key] === 0) {
+  //     return s.indexOf(key) === undefined ? 0 : s.indexOf(key);
+  //   }
+  // }
+  // return -1;
+};
+
+console.log(firstUniqChar('asdfagsdfasdf'));
