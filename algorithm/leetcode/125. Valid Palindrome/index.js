95914943694ef07c1817d400d76f356c7e13ea62
moshuying <1460083332@qq.com>
2019-11-19 23:35:41 +0800

    补充125题题解

---
+++ algorithm/leetcode/125. Valid Palindrome/index.js
@@ -0,0 +1,18 @@
+/**
+ * @Description :墨抒颖
+ * @Author :墨抒颖
+ * @Date :2019-11-19 22:44:17
+ * @LastEditTime :2019-11-19 22:44:29
+ * @LastEditors :墨抒颖
+ * @Github :https://github.com/moshuying
+ * @Gitee :https://gitee.com/moshuying
+ * @Blogs :https://blog.csdn.net/qq_34846662
+ * @Use :使用说明
+ */
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function (s) {
+
+};
