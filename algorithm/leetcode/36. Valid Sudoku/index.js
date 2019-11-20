0afe9dfacfcce5207e6df684fbaae47abfa7b2fb
moshuying <1460083332@qq.com>
2019-11-20 08:19:21 +0800

    去除26题无用注释,改进算法

--- algorithm/leetcode/36. Valid Sudoku/index.js
+++ algorithm/leetcode/36. Valid Sudoku/index.js
@@ -2,7 +2,7 @@
  * @Description :墨抒颖
  * @Author :墨抒颖
  * @Date :2019-11-10 22:25:48
- * @LastEditTime :2019-11-16 11:41:29
+ * @LastEditTime :2019-11-20 08:15:40
  * @LastEditors :墨抒颖
  * @Github :https://github.com/moshuying
  * @Gitee :https://gitee.com/moshuying
@@ -13,7 +13,6 @@
  * @param {character[][]} board
  * @return {boolean}
  */
-// eslint-disable-next-line complexity
 var isValidSudoku = function (board) {
   let [rows, columns, boxes] = [[], [], []];
   for (let i = 0; i < 9; i++) {
@@ -21,7 +20,7 @@ var isValidSudoku = function (board) {
     columns[i] = {}; // 列数据
     boxes[i] = {}; // 小宫格
   }
-  // const fun = (r, x, y) => (r[x][y] ? r[x][y]++ : (r[x][y] = 1));
+  const fun = (r, x, y) => (r[x][y] ? r[x][y]++ : (r[x][y] = 1));
   for (let i = 0; i < board.length; i++) {
     for (let j = 0; j < board[i].length; j++) {
       let num = board[i][j];
@@ -29,12 +28,9 @@ var isValidSudoku = function (board) {
         let n = parseInt(num, 10);
         // js中1/3*3计算结果为1 但是按理说应该是0.999无限(浮点误差) 而java中1/3*3则取值为0,浮点数默认舍去0
         let boxix = ~~((i / 3)) * 3 + ~~(j / 3);
-        // fun(rows, i, n);
-        // fun(columns, j, n);
-        // fun(boxes, boxix, n);
-        rows[i][n] ? rows[i][n]++ : (rows[i][n] = 1);
-        columns[j][n] ? columns[j][n]++ : (columns[j][n] = 1);
-        boxes[boxix][n] ? boxes[boxix][n]++ : (boxes[boxix][n] = 1);
+        fun(rows, i, n);
+        fun(columns, j, n);
+        fun(boxes, boxix, n);
         if (rows[i][n] >= 2 || columns[j][n] >= 2 || boxes[boxix][n] >= 2) {
           return false;
         }
