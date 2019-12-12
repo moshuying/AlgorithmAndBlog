948effc4902f36ff544fb7e8020ec0a46b48910f
moshuying <1460083332@qq.com>
2019-12-12 09:48:45 +0800

    14题解答

--- algorithm/leetcode/14. Longest Common Prefix/index.js
+++ algorithm/leetcode/14. Longest Common Prefix/index.js
@@ -2,7 +2,7 @@
  * @Description :墨抒颖
  * @Author :墨抒颖
  * @Date :2019-12-11 22:29:27
- * @LastEditTime :2019-12-11 22:44:36
+ * @LastEditTime :2019-12-11 22:48:17
  * @LastEditors :墨抒颖
  * @Github :https://github.com/moshuying
  * @Gitee :https://gitee.com/moshuying
@@ -14,17 +14,33 @@
  * @return {string}
  */
 var longestCommonPrefix = function (strs) {
+  strs.sort();
   if (strs.length === 0) {return '';}
-  let [defaultStr, tempStr, count] = ['', '', 0];
-  do {
-    tempStr = strs[0].charAt(count);
-    if (strs.every((el) => el.charAt(count) === tempStr)) {
-      defaultStr += tempStr;
-      count++;
-    } else {
-      tempStr = '';
+  let first = strs[0];
+  let end = strs[strs.length - 1];
+  let exp = new RegExp(`^${first}`);
+  // 对比第一个和最后一个，假如最后一个包含第一个，则返回第一个
+  if (first === end || end.match(exp)) {return first;}
+
+  // 假如不包含，则对比后得到最长的公共前缀
+  for (let i = 0; i < first.length; i++) {
+    if (first[i] !== end[i]) {
+      return first.slice(0, i);
     }
-  } while (tempStr);
-  return defaultStr;
+  }
+  // if (strs.length === 0) {return '';}
+  // let [defaultStr, tempStr, count] = ['', '', 0];
+  // do {
+  //   tempStr = strs[0].charAt(count);
+  //   // 用ervery去判断每一个数字
+  //   if (strs.every((el) => el.charAt(count) === tempStr)) {
+  //     defaultStr += tempStr;
+  //     count++;
+  //   } else {
+  //     // 空字符串为false
+  //     tempStr = '';
+  //   }
+  // } while (tempStr);
+  // return defaultStr;
 };
 console.log(longestCommonPrefix(['flower', 'flow', 'flight']));
