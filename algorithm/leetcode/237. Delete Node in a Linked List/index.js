22ea7b440a66fdeb5cf20a8c393fd2f7b171a0df
moshuying <1460083332@qq.com>
2019-12-19 20:31:34 +0800

    更新题解

---
+++ algorithm/leetcode/237. Delete Node in a Linked List/index.js
@@ -0,0 +1,31 @@
+/**
+ * @Description :墨抒颖
+ * @Author :墨抒颖
+ * @Date :2019-12-19 20:03:17
+ * @LastEditTime :2019-12-19 20:05:31
+ * @LastEditors :墨抒颖
+ * @Github :https://github.com/moshuying
+ * @Gitee :https://gitee.com/moshuying
+ * @Blogs :https://blog.csdn.net/qq_34846662
+ * @Use :使用说明
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+function ListNode (val) {
+  this.val = val;
+  this.next = null;
+}
+
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+var deleteNode = function (node) {
+  node.val = node.next.val;
+  node.next = node.next.next;
+};
